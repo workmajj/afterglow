95bcf8ae1e03252a92e74a7cd399471768965662
John J. Workman <johnjworkman@gmail.com>
2016-02-15 18:25:57 -0500

    Try out a bunch of images

--- glow.py
+++ glow.py
@@ -2,7 +2,8 @@
 
 from PIL import Image, ImageFilter
 
-im = Image.open("img/glow.jpg")
-im = im.resize((32, 32), resample=Image.LANCZOS)
-im = im.filter(ImageFilter.GaussianBlur(radius=2))
-im.show()
+for i in xrange(1, 37):
+    im = Image.open('img/glow' + str(i) + '.jpg')
+    im = im.resize((32, 32), resample=Image.LANCZOS)
+    im = im.filter(ImageFilter.GaussianBlur(radius=2))
+    im.save('img/glow' + str(i) + '-32.jpg', 'jpeg')
