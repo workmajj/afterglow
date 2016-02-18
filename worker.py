5ec34a542c2651ce41764df41a43d5ed50d36130
John J. Workman <johnjworkman@gmail.com>
2016-02-18 15:57:09 -0500

    Download images into tmp dir

--- worker.py
+++ worker.py
@@ -10,10 +10,19 @@ from bs4 import BeautifulSoup
 page = urllib2.urlopen('https://www.instagram.com/workmajj/')
 soup = BeautifulSoup(page, 'html.parser')
 
+# TODO: assert page structure
 anchors = soup.find_all('script', string=re.compile('window._sharedData'))
 content = anchors[0].contents[0]
 
-left = content.find('{');
-right = content.rfind('}');
+left = content.find('{')
+right = content.rfind('}')
+json = json.loads(content[left:right+1])
 
-print json.loads(content[left:right+1])
+try:
+    for node in json['entry_data']['ProfilePage'][0]['user']['media']['nodes']:
+        if node['is_video']:
+            continue
+        f = 'tmp/' + str(int(node['date'])) + '+' + str(node['code']) + '.jpg'
+        print urllib.urlretrieve(node['display_src'], f)
+except KeyError:
+    pass # TODO: assert json structure
