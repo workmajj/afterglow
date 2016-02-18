ff130e0b0370997f4c335dd689edee35fe81c9d1
John J. Workman <johnjworkman@gmail.com>
2016-02-18 15:46:04 -0500

    Extract and parse JSON

--- worker.py
+++ worker.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+import json
 import re
+import urllib
 import urllib2
 
 from bs4 import BeautifulSoup
@@ -8,5 +10,10 @@ from bs4 import BeautifulSoup
 page = urllib2.urlopen('https://www.instagram.com/workmajj/')
 soup = BeautifulSoup(page, 'html.parser')
 
-for anchor in soup.find_all('script', string=re.compile('window._sharedData')):
-    print anchor.contents[0]
+anchors = soup.find_all('script', string=re.compile('window._sharedData'))
+content = anchors[0].contents[0]
+
+left = content.find('{');
+right = content.rfind('}');
+
+print json.loads(content[left:right+1])
