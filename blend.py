945be5fb03f814a032d16a61f491096473d5495f
John J. Workman <johnjworkman@gmail.com>
2016-02-23 16:33:59 -0500

    Refactor blend

--- blend.py
+++ blend.py
@@ -1,23 +1,42 @@
 #!/usr/bin/env python
 
+import os
 import sys
 
 from PIL import Image, ImageFilter
 
+SIZE = (32, 32)
+
+def im_open_and_resize(fp):
+    im = Image.open(fp)
+    return im.resize(SIZE, resample=Image.LANCZOS)
+
+def basename_strip_ext(path):
+    ext = os.path.splitext(path)[1]
+    name = os.path.basename(path)
+    return name[:name.rfind(ext)]
+
 def main():
     if len(sys.argv) != 3:
-        sys.exit('usage: %s <image1> <image2>' % sys.argv[0])
+        sys.exit('usage: %s <im1> <im2>' % sys.argv[0])
 
-    im1 = Image.open(sys.argv[1])
-    im1 = im1.resize((32, 32), resample=Image.LANCZOS)
+    if not os.path.isfile(sys.argv[1]):
+        sys.exit('%s is not a regular file' % sys.argv[1])
+    if not os.path.isfile(sys.argv[2]):
+        sys.exit('%s is not a regular file' % sys.argv[2])
+    if os.path.samefile(sys.argv[1], sys.argv[2]):
+        sys.exit('files are the same')
 
-    im2 = Image.open(sys.argv[2])
-    im2 = im2.resize((32, 32), resample=Image.LANCZOS)
+    im1 = im_open_and_resize(sys.argv[1])
+    im2 = im_open_and_resize(sys.argv[2])
 
-    for i in xrange(5, 100, 5):
+    f1 = basename_strip_ext(sys.argv[1])
+    f2 = basename_strip_ext(sys.argv[2])
+
+    for i in xrange(1, 100):
         alpha = float(i) / 100
         out = Image.blend(im1, im2, alpha)
-        out.save('blend-' + '{:4.2f}'.format(alpha) + '.jpg') # FIXME
+        out.save(f1 + '-' + '{:4.2f}'.format(alpha) + '-' + f2 + '.jpg')
 
 if __name__ == "__main__":
     main()
