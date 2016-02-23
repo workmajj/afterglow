6be77251f3f803c28ba4f170ec3e6cb4a16a00a7
John J. Workman <johnjworkman@gmail.com>
2016-02-23 16:59:33 -0500

    Add tmp dir support to blend

--- blend.py
+++ blend.py
@@ -17,26 +17,30 @@ def basename_strip_ext(path):
     return name[:name.rfind(ext)]
 
 def main():
-    if len(sys.argv) != 3:
-        sys.exit('usage: %s <im1> <im2>' % sys.argv[0])
+    if len(sys.argv) != 4:
+        sys.exit('usage: %s <dir> <im1> <im2>' % sys.argv[0])
 
-    if not os.path.isfile(sys.argv[1]):
-        sys.exit('%s is not a regular file' % sys.argv[1])
+    if not os.path.isdir(sys.argv[1]):
+        sys.exit('%s is not a directory' % sys.argv[1])
     if not os.path.isfile(sys.argv[2]):
         sys.exit('%s is not a regular file' % sys.argv[2])
-    if os.path.samefile(sys.argv[1], sys.argv[2]):
+    if not os.path.isfile(sys.argv[3]):
+        sys.exit('%s is not a regular file' % sys.argv[3])
+    if os.path.samefile(sys.argv[2], sys.argv[3]):
         sys.exit('files are the same')
 
-    im1 = im_open_and_resize(sys.argv[1])
-    im2 = im_open_and_resize(sys.argv[2])
+    im1 = im_open_and_resize(sys.argv[2])
+    im2 = im_open_and_resize(sys.argv[3])
 
-    f1 = basename_strip_ext(sys.argv[1])
-    f2 = basename_strip_ext(sys.argv[2])
+    f1 = basename_strip_ext(sys.argv[2])
+    f2 = basename_strip_ext(sys.argv[3])
+
+    base = os.path.normpath(sys.argv[1]) + '/' + f1 + '-' + f2
 
     for i in xrange(1, 100):
         alpha = float(i) / 100
         out = Image.blend(im1, im2, alpha)
-        out.save(f1 + '-' + '{:4.2f}'.format(alpha) + '-' + f2 + '.jpg')
+        out.save(base + '-' + '{:4.2f}'.format(alpha) + '.jpg')
 
 if __name__ == "__main__":
     main()
