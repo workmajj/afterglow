08653913d31f8d85778cc7884bfbbf4804301c7b
John J. Workman <johnjworkman@gmail.com>
2016-02-23 13:54:49 -0500

    Delete resize

--- resize.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from PIL import Image, ImageFilter
-
-for i in xrange(1, 37):
-    im = Image.open('img/glow' + str(i) + '.jpg')
-    im = im.resize((32, 32), resample=Image.LANCZOS)
-    im = im.filter(ImageFilter.GaussianBlur(radius=2))
-    im.save('img/glow' + str(i) + '-32.jpg', 'jpeg')
