08a9405ce633b12a4b062727652ba5db9f1ce4e7
John J. Workman <johnjworkman@gmail.com>
2016-03-03 16:52:40 -0500

    Get dir listing of images to blend

--- blend.py
+++ blend.py
@@ -5,7 +5,12 @@ import sys
 
 from PIL import Image, ImageFilter
 
+JPEG = '.jpg'
 SIZE = (32, 32)
+
+def list_files_with_ext(dir_src, ext):
+    files = [os.path.join(dir_src, f) for f in os.listdir(dir_src)]
+    return [f for f in files if os.path.splitext(f)[1] == ext]
 
 def open_and_resize(path):
     im = Image.open(path)
@@ -17,30 +22,35 @@ def strip_ext(path):
     return name[:name.rfind(ext)]
 
 def main():
-    if len(sys.argv) != 4:
-        sys.exit('usage: %s <dir> <im1> <im2>' % sys.argv[0])
+    if len(sys.argv) != 3:
+        sys.exit('usage: {} <src_dir> <tmp_dir>'.format(sys.argv[0]))
 
-    if not os.path.isdir(sys.argv[1]):
-        sys.exit('%s is not a directory' % sys.argv[1])
-    if not os.path.isfile(sys.argv[2]):
-        sys.exit('%s is not a regular file' % sys.argv[2])
-    if not os.path.isfile(sys.argv[3]):
-        sys.exit('%s is not a regular file' % sys.argv[3])
-    if os.path.samefile(sys.argv[2], sys.argv[3]):
-        sys.exit('files are the same')
+    for i in (1, 2):
+        if not os.path.isdir(sys.argv[i]):
+            sys.exit('{} is not a directory'.format(sys.argv[i]))
 
-    im1 = open_and_resize(sys.argv[2])
-    im2 = open_and_resize(sys.argv[3])
+    dir_src = os.path.normpath(sys.argv[1])
+    dir_tmp = os.path.normpath(sys.argv[2])
 
-    # format: <im1_no_ext>-<im2_no_ext>-<alpha_as_pct>.jpg
+    images = sorted(list_files_with_ext(dir_src, JPEG))
+    if not images:
+        sys.exit('no {} files in {}'.format(JPEG, dir_src))
 
-    prefix = os.path.normpath(sys.argv[1]) + '/' + \
-        strip_ext(sys.argv[2]) + '-' + strip_ext(sys.argv[3]) + '-'
+    for curr, _ in enumerate(images):
+        next = curr + 1 if curr + 1 < len(images) else 0
 
-    for i in xrange(1, 100):
-        alpha = float(i) / 100
-        out = Image.blend(im1, im2, alpha)
-        out.save(prefix + '{:0>2d}.jpg'.format(i))
+        im1 = open_and_resize(images[curr])
+        im2 = open_and_resize(images[next])
+
+        # format: <im1_no_ext>-<im2_no_ext>-<alpha_as_pct>.jpg
+
+        prefix = dir_tmp + '/' + \
+            strip_ext(images[curr]) + '-' + strip_ext(images[next]) + '-'
+
+        for i in xrange(0, 100):
+            alpha = float(i) / 100
+            out = Image.blend(im1, im2, alpha)
+            out.save(prefix + '{:0>2d}.jpg'.format(i))
 
 if __name__ == '__main__':
     main()
